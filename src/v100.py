9fd69d7f6d7989ef709b44a21678254c8da9d040
numb3r303 <numb3r303@gmail.com>
2018-08-12 23:47:11 +0530

    Fix typo.

--- src/v100.py
+++ src/v100.py
@@ -1765,7 +1765,7 @@ if __name__ == '__main__':
             
         else:
             print('Lets find hyper-parameters ...')
-            best_score, best_params = m.optimize_lgb(train, tests, feature_list)
+            best_score, best_params = m.optimize_lgb(train, test, feature_list)
             
             if not is_sample:
                 joblib.dump(best_score, os.path.join(basepath, output_path + f'{model_identifier}_best_score.pkl'))
