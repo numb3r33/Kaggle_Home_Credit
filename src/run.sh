ec3dbbdef185f5412c45d257dc8e865372cc894c
numb3r303 <numb3r303@gmail.com>
2018-08-24 17:14:27 +0530

    Update run command.

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python v127.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1234
-python v128.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1234
+python v129.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
+python v129.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1234
