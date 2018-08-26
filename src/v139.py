2f0f849fe391b133b6ba0ab26421e47cbe975649
numb3r303 <numb3r303@gmail.com>
2018-08-26 20:16:45 +0530

    Remove knn features.

--- src/v139.py
+++ src/v139.py
@@ -1963,50 +1963,6 @@ if __name__ == '__main__':
             feature_list = list(set(feature_list) - set(COLS_TO_REMOVE))
             np.save(os.path.join(basepath, output_path + f'{data_folder}{MODEL_FILENAME}_features.npy'), feature_list)
 
-        # KNN Features
-        if os.path.exists(os.path.join(basepath, output_path + f'{data_folder}knn_features.pkl')):
-            print('Loading KNN features ...')
-            knn_features = joblib.load(os.path.join(basepath, output_path + f'{data_folder}knn_features.pkl'))
-        else:
-            print('Preparing knn features ...')
-
-            t0        = time.time()
-            data_copy = data.copy()
-            data_copy = fill_missing_values(data_copy)
-
-            train = data_copy.iloc[:m.n_train]
-            test  = data_copy.iloc[m.n_train:]
-
-            X = train.loc[:, feature_list].values
-            y = train.loc[:, 'TARGET'].astype(np.uint8).values
-
-            X_test = test.loc[:, feature_list].values
-
-            # standardization
-            scaler = MinMaxScaler()
-            X      = scaler.fit_transform(X)
-            X_test = scaler.transform(X_test)
-
-            nn_features        = NearestNeighborsFeatures(n_neighbors=5, metric='cosine', k_list=[3, 5, 7], n_jobs=16)
-            knn_train_features = cross_val_predict(nn_features, X, y, cv=5)
-
-            nn_features.fit(X, y)
-            knn_test_features = nn_features.predict(X_test)
-
-            joblib.dump(knn_train_features, os.path.join(basepath, output_path + f'{data_folder}knn_train_features.pkl'))
-            joblib.dump(knn_test_features, os.path.join(basepath, output_path + f'{data_folder}knn_test_features.pkl'))
-
-            knn_features = np.vstack((knn_train_features, 
-                                      knn_test_features))
-            joblib.dump(knn_features, os.path.join(basepath, output_path + f'{data_folder}knn_features.pkl'))
-
-            print('Took: {} seconds to generate knn features'.format({time.time() - t0}))
-
-        # knn features
-        knn_features = pd.DataFrame(knn_features, columns=[f'knn_{i}' for i in range(knn_features.shape[1])])
-        knn_features.index = data.index
-
-        data  = pd.concat((data, knn_features), axis=1)     
         train = data.iloc[:m.n_train]
 
         del data
@@ -2024,7 +1980,6 @@ if __name__ == '__main__':
         cv_score   = str(cv_history.iloc[-1]['test-auc-mean']) + '_' + str(cv_history.iloc[-1]['test-auc-std'])
         
         PARAMS['num_boost_round'] = np.argmax(cv_history['test-auc-mean'].values) + 1
-
 
         print('*' * 100)
         print('Best AUC: {}'.format(cv_score))
@@ -2089,7 +2044,7 @@ if __name__ == '__main__':
             np.save(os.path.join(basepath, output_path + f'{data_folder}{MODEL_FILENAME}_features.npy'), feature_list)
 
         
-        PARAMS['seed']                  = SEED
+        PARAMS['seed'] = SEED
         
         if os.path.exists(os.path.join(basepath, output_path + f'{data_folder}{MODEL_FILENAME}_{CV_SEED}_test_preds.npy')):
             oof_train_preds  = np.load(os.path.join(basepath, output_path + f'{data_folder}{MODEL_FILENAME}_{CV_SEED}_oof_train_preds.npy'))       
