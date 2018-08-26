49df645dc1a0a577c1ae29537a7f169642ad1a6a
numb3r303 <numb3r303@gmail.com>
2018-08-26 20:06:02 +0530

    New model xgboost.

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python v138.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
-python v138.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1231
+python v139.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
+python v139.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1231
