b5f4fcc46b574d2a9aa93a01d77cb62dc199b41e
numb3r303 <numb3r303@gmail.com>
2018-08-27 19:56:34 +0530

    Update command

--- src/run.sh
+++ src/run.sh
@@ -12,5 +12,5 @@
 # python v142.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
 # python v142.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1231
 # LGB ( null importance )
-python v145.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -s True -seed 4457
+python v145.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
 # python v145.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1231
