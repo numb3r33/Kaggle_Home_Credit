37daeb52942db424f03c998bb061376fc8d3fda0
numb3r303 <numb3r303@gmail.com>
2018-08-27 21:58:00 +0530

    Ensemble different params.

--- src/run.sh
+++ src/run.sh
@@ -13,4 +13,7 @@
 # python v142.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1231
 # LGB ( null importance )
 # python v145.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
-python v145.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1231
+# python v145.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1231
+# Ensemble
+python v146.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
+# python v146.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -t True -cv_seed 4457 -seed 1235
