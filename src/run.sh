94667df9e5366c6e88201d0658b6ff5192726693
numb3r303 <numb3r303@gmail.com>
2018-08-27 23:54:18 +0530

    Update run command.

--- src/run.sh
+++ src/run.sh
@@ -18,4 +18,4 @@
 # python v146.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
 # python v146.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -t True -cv_seed 4457 -seed 1235
 # LGB (dart)
-# python v147.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
+python v147.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
