ea5a4bb2b4f98a39b50d3f3d83c4a94a8ee4ee2b
numb3r303 <numb3r303@gmail.com>
2018-08-28 18:36:13 +0530

    Update run command.

--- src/run.sh
+++ src/run.sh
@@ -43,5 +43,5 @@
 
 
 # Stacker ( 1st-stage )
-python v152.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
-# python v152.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -t True -cv_seed 4457 -seed 1235
+# python v152.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
+python v152.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -t True -cv_seed 4457 -seed 1235
