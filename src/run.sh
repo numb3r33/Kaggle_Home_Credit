8e41964f0befde4be6625325d168a4f29209abfc
numb3r303 <numb3r303@gmail.com>
2018-08-28 19:06:08 +0530

    Update run command.

--- src/run.sh
+++ src/run.sh
@@ -2,7 +2,7 @@
 
 # CatBoost
 # python v120.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
-# python v120.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1231
+python v120.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1231
 
 # XGBoost
 # python v143.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
@@ -44,4 +44,4 @@
 
 # Stacker ( 1st-stage )
 # python v152.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
-python v152.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -t True -cv_seed 4457 -seed 1235
+# python v152.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -t True -cv_seed 4457 -seed 1235
