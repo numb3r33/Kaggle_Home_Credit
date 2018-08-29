7304e8cf5e9dc082f3c5df523dd0bd868c379148
numb3r303 <numb3r303@gmail.com>
2018-08-29 16:34:45 +0530

    Add new stacker and 1st-stage stacker.

--- src/run.sh
+++ src/run.sh
@@ -39,7 +39,7 @@
 
 
 # LGB ( cv_adversarial_idx_v1.csv, gain_score > .3 )
-# python v151.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1231
+python v151.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1231
 
 
 # Stacker ( 1st-stage )
@@ -47,4 +47,9 @@
 # python v152.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -t True -cv_seed 4457 -seed 1235
 
 # LGB ( XGBOOST Leaves )
-python v153.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1231
+# python v153.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv_predict True -cv_seed 4457 -seed 1231
+
+
+# Stacker ( 1st-stage )
+python v154.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -cv True -seed 4457
+python v154.py -input_path data/raw/ -output_path data/interim/ -data_folder dataset6/ -t True -cv_seed 4457 -seed 1235
